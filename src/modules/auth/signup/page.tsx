c758b09625b9c8fe16bc51bb6622a0337aad1eb3
Uthman Abdulganiyu <uthmanabdulganiyu2019@gmail.com>
2023-04-03 00:52:56 +0100

    [not-tested] implement forget password

--- src/modules/auth/signup/page.tsx
+++ src/modules/auth/signup/page.tsx
@@ -86,15 +86,6 @@ export const SignUpPage = () => {
                     Remember me
                   </label>
                 </div>
-
-                <div className="text-sm">
-                  <a
-                    href="#"
-                    className="font-medium text-indigo-600 hover:text-indigo-500"
-                  >
-                    Forgot your password?
-                  </a>
-                </div>
               </div>
 
               <Button
