e3241386fa3b7f237512006a335cb2c31f222f7a
Uthman Abdulganiyu <uthmanabdulganiyu2019@gmail.com>
2023-04-04 22:52:25 +0100

    added toastify package

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import type { AppProps } from "next/app";
 import { SessionProvider } from "next-auth/react";
 import type { Session } from "next-auth";
+import { ToastContainer } from "react-toastify";
 
 export default function App({
   Component,
@@ -10,6 +11,18 @@ export default function App({
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
+      <ToastContainer
+        position="top-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </SessionProvider>
   );
 }
